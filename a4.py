fcf99db3f52fa745510caaf4dc5d2bd09c8c0c45
wasobi <sierraobi@my.uri.edu>
2019-04-25 14:12:22 -0400

    create file for assignment

---
+++ a4.py
@@ -0,0 +1,12 @@
+"""
+1. Define a function to count kmers of size k, where k is specified as an argument.
+2. Define a function to create a pandas data frame containing all possible k and the associated
+number of observed and expected kmers (see above table).
+3. Define a function to produce a graph from the data frame of the proportion of each kmer
+observed.
+4. Define a function to calculate linguistic complexity.
+5. Write a script to thoroughly test each of your functions.
+6. Use the main function to read in your sequence data and output results.
+7. Create a github repository including a README (in markdown) to submit your work. Be sure that
+all your functions have appropriate docstrings.
+"""
